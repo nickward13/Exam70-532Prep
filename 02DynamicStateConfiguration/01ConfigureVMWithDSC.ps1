acfa6c158bdbc14dea41fea44160919c568cb40b
Nick Ward <nickward@microsoft.com>
2016-11-29 11:56:44 +1100

    Added DSC Configuration

---
+++ 02DynamicStateConfiguration/01ConfigureVMWithDSC.ps1
@@ -0,0 +1,21 @@
+param(
+    [string]$ResourceGroupName = "myResourceGroup",
+    [string]$VMName = "myVM",
+    [string]$ConfigurationName = "IisInstall",
+    [string]$StorageAccountName = "hectagonstorage23"
+)
+
+#Publish the configuration script to user storage
+Write-Host "Publishing configuration script '$ConfigurationPath' to '$StorageAccountName'"
+$ConfigurationPath = ".\" + $ConfigurationName + ".ps1"
+Publish-AzureRmVMDscConfiguration -ResourceGroupName $ResourceGroupName -ConfigurationPath $ConfigurationPath -ContainerName "config" -StorageAccountName $StorageAccountName -Verbose -Force
+
+# Set the VM to run the DSC configuration
+$ArchiveBlobName = $ConfigurationName + ".ps1.zip"
+Write-Host "Setting DSC Extension from $ArchiveBlobName on VM $VMName"
+Set-AzureRmVMDscExtension -ResourceGroupName $ResourceGroupName -VMName $VMName -ArchiveBlobName $ArchiveBlobName -ArchiveStorageAccountName $StorageAccountName -ArchiveContainerName "config" -Version "2.20" -Verbose -ConfigurationName "IISInstall"
+
+# Check DSC Status
+Get-AzureRmVMDscExtension -ResourceGroupName $ResourceGroupName -VMName $VMName -Verbose
+
+
