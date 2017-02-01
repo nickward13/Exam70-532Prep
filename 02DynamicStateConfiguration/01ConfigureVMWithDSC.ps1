78a5640c8fb6e856b42f73e2269120ea9363c9d0
Nick Ward <nickward@microsoft.com>
2017-02-02 10:37:46 +1100

    Format DSC script for split-screen

--- 02DynamicStateConfiguration/01ConfigureVMWithDSC.ps1
+++ 02DynamicStateConfiguration/01ConfigureVMWithDSC.ps1
@@ -8,14 +8,19 @@ $StorageAccountName = "hect" + $ResourceGroupName.ToLower()
 #Publish the configuration script to user storage
 $ConfigurationPath = ".\" + $ConfigurationName + ".ps1"
 Write-Host "Publishing configuration script '$ConfigurationPath' to '$StorageAccountName'"
-Publish-AzureRmVMDscConfiguration -ResourceGroupName $ResourceGroupName -ConfigurationPath $ConfigurationPath -ContainerName "config" -StorageAccountName $StorageAccountName -Verbose -Force
+Publish-AzureRmVMDscConfiguration -ResourceGroupName $ResourceGroupName `
+    -ConfigurationPath $ConfigurationPath -ContainerName "config" `
+    -StorageAccountName $StorageAccountName -Verbose -Force
 
 # Set the VM to run the DSC configuration
 $ArchiveBlobName = $ConfigurationName + ".ps1.zip"
 Write-Host "Setting DSC Extension from $ArchiveBlobName on VM $VMName"
-Set-AzureRmVMDscExtension -ResourceGroupName $ResourceGroupName -VMName $VMName -ArchiveBlobName $ArchiveBlobName -ArchiveStorageAccountName $StorageAccountName -ArchiveContainerName "config" -Version "2.20" -Verbose -ConfigurationName "IISInstall"
+Set-AzureRmVMDscExtension -ResourceGroupName $ResourceGroupName `
+    -VMName $VMName -ArchiveBlobName $ArchiveBlobName `
+    -ArchiveStorageAccountName $StorageAccountName `
+    -ArchiveContainerName "config" -Version "2.20" `
+    -Verbose -ConfigurationName "IISInstall"
 
 # Check DSC Status
-Get-AzureRmVMDscExtension -ResourceGroupName $ResourceGroupName -VMName $VMName -Verbose
-
-
+Get-AzureRmVMDscExtension -ResourceGroupName $ResourceGroupName `
+    -VMName $VMName -Verbose
