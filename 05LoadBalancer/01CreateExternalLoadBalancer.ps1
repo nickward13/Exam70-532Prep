d2f4738212724736309997b467ada391a893fa05
Nick Ward <nickward@microsoft.com>
2017-02-06 15:49:31 +1100

    corrected order of health probe and rule creation

--- 05LoadBalancer/01CreateExternalLoadBalancer.ps1
+++ 05LoadBalancer/01CreateExternalLoadBalancer.ps1
@@ -10,9 +10,9 @@ $publicIPAddress = New-AzureRmPublicIpAddress -ResourceGroupName $myResourceGrou
 
  $frontendIP = New-AzureRmLoadBalancerFrontendIpConfig -Name LB-Frontend -PublicIpAddress $publicIPAddress
  $beaddresspool = New-AzureRmLoadBalancerBackendAddressPoolConfig -Name LB-backend
- $lbrule = New-AzureRmLoadBalancerRuleConfig -Name HTTP -FrontendIpConfiguration $frontendIP -BackendAddressPool  $beAddressPool -Probe $healthProbe -Protocol Tcp -FrontendPort 80 -BackendPort 80
  $healthProbe = New-AzureRmLoadBalancerProbeConfig -Name HealthProbe -Protocol Tcp -Port 80 -IntervalInSeconds 15 -ProbeCount 2
-
+ $lbrule = New-AzureRmLoadBalancerRuleConfig -Name HTTP -FrontendIpConfiguration $frontendIP -BackendAddressPool  $beAddressPool -Probe $healthProbe -Protocol Tcp -FrontendPort 80 -BackendPort 80
+ 
 Write-Host "Creating load balancer..."
  $NRPLB = New-AzureRmLoadBalancer -ResourceGroupName $myResourceGroup -Name ExternalLB -Location $location -FrontendIpConfiguration $frontendIP `
     -BackendAddressPool $beAddressPool -LoadBalancingRule $lbrule -Probe $healthProbe
